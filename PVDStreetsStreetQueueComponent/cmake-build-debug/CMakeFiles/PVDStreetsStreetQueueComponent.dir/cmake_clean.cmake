faf476e069ca3af93b9ac3e3249c6917ed23b77a
Jared Simmons <simmons.jared76@gmail.com>
2019-05-09 12:57:11 -0400

    Please work

---
+++ PVDStreetsStreetQueueComponent/cmake-build-debug/CMakeFiles/PVDStreetsStreetQueueComponent.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/PVDStreetsStreetQueueComponent.dir/main.c.obj"
+  "PVDStreetsStreetQueueComponent.pdb"
+  "PVDStreetsStreetQueueComponent.exe"
+  "PVDStreetsStreetQueueComponent.exe.manifest"
+  "libPVDStreetsStreetQueueComponent.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/PVDStreetsStreetQueueComponent.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
